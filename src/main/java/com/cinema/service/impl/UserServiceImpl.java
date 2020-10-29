46cf6762a6a90e0ac4b20c54527368ceb08eeac5
FYarkiy <flx_jb@yahoo.com>
2020-10-29 17:35:06 +0200

    remove unused import from userService

--- src/main/java/com/cinema/service/impl/UserServiceImpl.java
+++ src/main/java/com/cinema/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import com.cinema.dao.UserDao;
 import com.cinema.model.User;
 import com.cinema.service.UserService;
 import java.util.Optional;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
